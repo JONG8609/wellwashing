9c1d2113ed2c7ac403f33c135192a7b2e0167f20
JongJin Oh <dhwhdwls2@naver.com>
2025-03-22 16:46:01 +0900

    29

--- popup.js
+++ popup.js
@@ -25,27 +25,27 @@ const popupData = [
     
     {
         title: "대전 갑천 트리플시티 힐스테이트",
-        image: "src\images\open_pictures\daejeon_gabcjeon.png",
+        image: "src/images/open_pictures/daejeon_gabcjeon.png",
         description: "도산대로점 오픈",
     },
     {
         title: "인천 송도 레이크시티 힐스테이트 3차",
-        image: "src\images\open_pictures\songdo_lake_3.png",
+        image: "src/images/open_pictures/songdo_lake_3.png",
         description: "강남점 신규 오픈!",
     },
     {
         title: "인천 학익동 시티오씨엘 1차",
-        image: "src\images\open_pictures\hakik1.png",
+        image: "src/images/open_pictures/hakik1.png",
         description: "신촌점 신규 오픈!",
     },
     {
         title: "인천 학익동 시티오씨엘 3차",
-        image: "src\images\open_pictures\hakik3.png",
+        image: "src/images/open_pictures/hakik3.png",
         description: "서초점 신규 오픈!",
     },
     {
         title: "서울 관악구 힐스테이트 관악 쎈트씨엘",
-        image: "src\images\open_pictures\gwanak1.png",
+        image: "src/images/open_pictures/gwanak1.png",
         description: "서초점 신규 오픈!",
     },
     {
